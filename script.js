84d60fbd1253bfd8c14c98dbfbda6ffdef9401b3
Alex Lopes <lopes.alex.antonio@gmail.com>
2021-02-16 14:18:31 -0300

    Add JQuery

--- script.js
+++ script.js
@@ -11,15 +11,16 @@ $(document).ready(function (e) {
 
     $('area').on('focus', function (e) {
         e.preventDefault();
+        alert($(this).attr('class'))
         $('.selection p').html($(this).attr('class'));
     });
 
-    $(document).on('click', function (e) {
-        e.preventDefault();
-        if ($(e.target).closest('area').length === 0) {
-            $('.selection p').html('click a brick');
-        }
-    });
+    // $(document).on('click', function (e) {
+    //     e.preventDefault();
+    //     if ($(e.target).closest('area').length === 0) {
+    //         $('.selection p').html('click a brick');
+    //     }
+    // });
 
 
 
